0926a326df0b5af9ea89f19999106348d52afdce
CLARKE KEATON E <kcla1801@education.mpls.k12.mn.us>
2018-01-22 14:29:00 -0600

    We have everything we want. We still want to combine all both methods we have learned about. But if we havent don't we still have two working examples

--- Q2 Final Project.py
+++ Q2 Final Project.py
@@ -1,7 +1,7 @@
 # Q2 Project 
 import re #regualr expersion operations
 import math
-'''
+
 def solve_lq ( equ ):
   
     match = re.match("(\d+)x\+(\d+)=(\d+)", equ) #\d range of numbbers between 0 and 9 for the coefficent, constant and number problem consits of
@@ -9,7 +9,8 @@ def solve_lq ( equ ):
     m, b, y = float(m), float(b), float(y) # Convert from strings to numbers
     x = (y-b)/m
     print ("x = %f" % x) #%f acts as a format specifier, holds the variable.
-'''
+
+
 
 a,b,c = input("Enter the coefficients of a, b and c separated by commas: ")
 
@@ -22,5 +23,15 @@ elif d == 0:
     print "This equation has one solutions: ", x
 else:
     x1 = (-b+math.sqrt(d))/ (2*a)
-    x2 = (-b-math.sqrt(d))/( 2*a)
+    x2 = (-b-math.sqrt(d))/ (2*a)
     print "This equation has two solutions: ", x1, " and", x2
+
+''' 
+    
+m,b,y = input("Enter the coefficients of m, b and y separated by commas: ")
+match = re.match("(\d+)x\+(\d+)=(\d+)")
+m, b, y = float(m), float(b), float(y)
+if y == y:
+    x = (y-b)/m
+    print "This equation has one solutions: ", x
+'''
